7ccb8cb4ba5f4ccde41450274473a70f9793a773
Psiopteryx <philip.hahn@kensq.com>
2019-05-03 18:46:40 +0100

    hg

--- redqueen_gan.py
+++ redqueen_gan.py
@@ -1,1 +1,205 @@
-import keras
+import time, os, sys, humanfriendly, warnings, subprocess
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from keras.models import Sequential
+from keras.layers import Conv2D, Conv2DTranspose, Reshape
+from keras.layers import Flatten, BatchNormalization, Dense, Activation
+from keras.layers.advanced_activations import LeakyReLU
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+import scratch
+
+source_directory = 'd:/birds/'
+results_directory = 'd:/results/'
+
+
+image_size = 64
+batch_size = 64
+
+discriminator_learn_rate = 0.0002
+generator_learn_rate = 0.0002
+
+epochs = 200
+leaky_ReLU = 0.2
+label_noise = 0.2  # True/false noise for discriminator
+
+def load_dataset(source_directory, batch_size, image_shape):
+    dataset = ImageDataGenerator()
+    dataset = dataset.flow_from_directory(source_directory, target_size=(image_shape[0], image_shape[1]),
+                                          batch_size=batch_size, class_mode=None)
+    return dataset, dataset.samples
+
+def build_discriminator(image_shape):
+    discriminator = Sequential()
+    discriminator.add(Conv2D(filters=64, kernel_size=(5,5), strides=(2,2), padding='same',
+                data_format='channels_last',  kernel_initializer='glorot_uniform', input_shape=(image_shape)))
+    discriminator.add(LeakyReLU(leaky_ReLU))
+
+    discriminator.add(Conv2D(filters=128, kernel_size=(5, 5), strides=(2, 2), padding='same',
+                                data_format='channels_last', kernel_initializer='glorot_uniform'))
+    discriminator.add(BatchNormalization(momentum=0.5))
+    discriminator.add(LeakyReLU(leaky_ReLU))
+
+    discriminator.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(2, 2), padding='same',
+                                data_format='channels_last', kernel_initializer='glorot_uniform'))
+    discriminator.add(BatchNormalization(momentum=0.5))
+    discriminator.add(LeakyReLU(leaky_ReLU))
+
+    discriminator.add(Conv2D(filters=512, kernel_size=(5, 5), strides=(2, 2), padding='same',
+                                data_format='channels_last', kernel_initializer='glorot_uniform'))
+    discriminator.add(BatchNormalization(momentum=0.5))
+    discriminator.add(LeakyReLU(leaky_ReLU))
+
+    discriminator.add(Flatten())
+    discriminator.add(Dense(1))
+    discriminator.add(Activation('sigmoid'))
+    optimizer = Adam(discriminator_learn_rate, beta_1=0.1)
+    discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=None)
+
+    return discriminator
+
+def build_generator():
+    generator = Sequential()
+    generator.add(Dense(units=4 * 4 * 512, kernel_initializer='glorot_uniform', input_shape=(1, 1, 100)))
+    generator.add(Reshape(target_shape=(4, 4, 512)))
+    generator.add(BatchNormalization(momentum=0.5))
+    generator.add(Activation('relu'))
+
+    generator.add(Conv2DTranspose(filters=256, kernel_size=(5,5), strides=(2,2), padding='same',
+                                  data_format='channels_last', kernel_initializer='glorot_uniform'))
+    generator.add(BatchNormalization(momentum=0.5))
+    generator.add(Activation('relu'))
+
+    generator.add(Conv2DTranspose(filters=128, kernel_size=(5,5), strides=(2,2), padding='same',
+                                  data_format='channels_last', kernel_initializer='glorot_uniform'))
+    generator.add(BatchNormalization(momentum=0.5))
+    generator.add(Activation('relu'))
+
+    generator.add(Conv2DTranspose(filters=64, kernel_size=(5,5), strides=(2,2), padding='same',
+                                  data_format='channels_last', kernel_initializer='glorot_uniform'))
+    generator.add(BatchNormalization(momentum=0.5))
+    generator.add(Activation('relu'))
+
+    generator.add(Conv2DTranspose(filters=3, kernel_size=(5,5), strides=(2,2), padding='same',
+                                  data_format='channels_last', kernel_initializer='glorot_uniform'))
+    generator.add(Activation('tanh'))
+
+    optimizer = Adam(lr=generator_learn_rate, beta_1=0.5)
+    generator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=None)
+
+    return generator
+
+def save_generated_images(generated_images, epoch, batch_number):
+    plt.figure(figsize=(8,8), num=2)
+    gs1 = gridspec.GridSpec(8,8)
+    gs1.update(wspace=0, hspace=0)
+
+    for i in range(64):
+        ax1 = plt.subplot(gs1[i])
+        ax1.set_aspect('equal')
+        image = generated_images[i, :, :, :]
+        image +=1
+        image *= 127.5
+        fig = plt.imshow(image.astype(np.uint8))
+        plt.axis('off')
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+
+    plt.tight_layout()
+    save_name = 'generated images / generated samples_epoch' + \
+                str(epoch + 1) + '_batch_' +str(batch_number + 1) + '.png'
+    plt.savefig(save_name, bbox_inches='tight', pad_inches=0)
+    plt.pause(0.000000000001)
+    plt.show()
+
+def train_dcgan(batch_size, epochs, image_shape, dataset_path):
+    generator = build_generator()
+    discriminator = build_discriminator(image_shape)
+
+    gan = Sequential()
+    discriminator.trainable = False  # Only for adversarial model
+    gan.add(generator)
+    gan.add(discriminator)
+    optimizer = Adam(lr=generator_learn_rate, beta_1=0.5)
+    gan.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=None)
+
+    image_shape = (image_size, image_size, 3)
+    dataset, num_images = load_dataset(source_directory, batch_size, image_shape)
+    num_batches = int(num_images / batch_size)
+
+    adversarial_loss = np.empty(shape=1)
+    discriminator_loss = np.empty(shape=1)
+    batches = np.empty(shape=1)
+    plt.ion()
+    batch_count = 0
+
+    for epoch in range(epochs):
+        print("Epoch: " + str(epoch+1) + "/" + str(epochs) + " :")
+        for batch_number in range(num_batches):
+            batch_start_time = time.time()
+            real_images = dataset.next()
+            real_images /= 127.5  # normalise
+            real_images -= 1
+            current_batch_size = real_images.shape[0]  # for final batch (smaller)
+
+            noise = np.random.normal(0, 1, size=(current_batch_size,) + (1,1,100))
+            generated_images = generator.predict(noise)
+
+            # introduce noise to discriminator True/False label
+            real_y = (np.ones(current_batch_size) - np.random.random_sample(current_batch_size) * label_noise)
+            fake_y = np.random.random_sample(current_batch_size) * label_noise
+
+            # train discriminator
+            discriminator.trainable = True
+            d_loss = discriminator.train_on_batch(real_images, real_y)
+            d_loss += discriminator.train_on_batch(generated_images, fake_y)
+            discriminator_loss = np.append(discriminator_loss, d_loss)
+
+            # train generator
+            discriminator.trainable = False
+            noise = np.random.normal(0, 1, size=(current_batch_size * 2,) + (1, 1, 100))
+
+            # noise to discriminator True/False label again
+            fake_y = (np.ones(current_batch_size * 2) - np.random.random_sample(current_batch_size *2) * label_noise)
+
+            g_loss = gan.train_on_batch(noise, fake_y)
+            adversarial_loss = np.append(adversarial_loss, g_loss)
+            batches = np.append(batches, batch_count)
+
+            if((batch_number + 1) % 50 == 0 and current_batch_size == batch_size):
+                save_generated_images(generated_images, epoch, batch_number)
+
+            time_elapsed = time.time() - start_time
+
+            print("Batch: " + str(batch_number + 1) + "/" + str(num_batches)
+                  + "\tGenerator loss: " + str(round(g_loss, 3)) + "\tDscriminator loss: " + str(round(d_loss, 3))
+                  + '\tBatch time: ' + str(round(time_elapsed, 3)) + ' sec')
+
+            batch_count += 1
+
+        plt.figure(1)
+        plt.plot(batches, adversarial_loss, color='green', label='Generator Loss')
+        plt.plot(batches, discriminator_loss, color='blue', label='Discriminator Loss')
+        plt.title("RedQueen Results")
+        plt.xlabel("Batch Number")
+        plt.ylabel("Loss")
+        if epoch == 0:
+            plt.legend()
+        plt.pause(0.0000000001)
+        plt.show()
+        plt.savefig('trainingLossPlot.png')
+
+def main():
+    image_shape = (64, 64, 3)
+    train_dcgan(batch_size, epochs, image_shape, source_directory)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    duration = humanfriendly.format_timespan(round(end_time - start_time, 1))
+    print("\nRuntime for GAN: " + str(duration))
