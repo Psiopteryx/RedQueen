92c041d4a25c9abf3d72953d6f0b60b5c9f96fbc
Psiopteryx <4976259+Psiopteryx@users.noreply.github.com>
2020-07-15 23:58:38 +0100

    Update gputest.py

--- gputest.py
+++ gputest.py
@@ -1,13 +1,13 @@
 import os, torch, time
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # kill compiler warnings
-from keras import backend
+import tensorflow as tf
+
 
 def main():
-    if len(backend.tensorflow_backend._get_available_gpus()) > 0:
-        print("\nNumber of Keras-enabled GPUs: ", len(backend.tensorflow_backend._get_available_gpus()))
-    else: print("No Keras-enabled GPUs detected")
+    print("\nNumber of Tensorflow GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+ #   else: print("No Keras-enabled GPUs detected")
     if torch.cuda.device_count() > 0:
-        print("Number of Torch-enabled GPUs: ", torch.cuda.device_count())
+        print("\nNumber of Torch-enabled GPUs: ", torch.cuda.device_count())
         print("\nGPU Name: ", torch.cuda.get_device_name(0))
     else: print("No Torch-enabled GPUs detected")
 
